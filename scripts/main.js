677645309b04aef0e3e3b569d2baac02ccc18b00
Hiroki Kihara <fj8550jf@aa.jp.fujitsu.com>
2015-10-30 14:19:58 +0900

    push message

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 (function () {
   'use strict';
   var milkcocoa = new MilkCocoa("juiceig779397.mlkcca.com");
+  var dataStore = milkcocoa.dataStore("chat");
+  var me, message_text;
+
+  window.onload = function(){
+    me = this;
+    me.message_text = document.getElementById("message_text");
+  };
+
+  document.getElementById("btn").onclick = function(e){
+      var text = me.message_text.value;
+      dataStore.push({ message : text });
+  };
 })();
