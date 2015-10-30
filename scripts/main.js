c5878cd69eaac2507cb4161a30723a892f4e0c65
Hiroki Kihara <fj8550jf@aa.jp.fujitsu.com>
2015-10-30 14:27:51 +0900

    receive message

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,26 @@
   'use strict';
   var milkcocoa = new MilkCocoa("juiceig779397.mlkcca.com");
   var dataStore = milkcocoa.dataStore("chat");
-  var me, message_text;
+  var me, message_text, message_board;
 
   window.onload = function(){
     me = this;
     me.message_text = document.getElementById("message_text");
+    me.message_board = document.getElementById("message_board");
   };
 
   document.getElementById("btn").onclick = function(e){
-      var text = me.message_text.value;
-      dataStore.push({ message : text });
+    var text = me.message_text.value;
+    dataStore.push({ message : text });
   };
+
+  dataStore.on("push",function(d){
+    onMessage(d.value.message);
+  });
+
+  function onMessage(messageText){
+    var boardDom = document.createElement("li");
+    boardDom.innerHTML = messageText;
+    me.message_board.insertBefore(boardDom, me.message_board.firstChild);
+  }
 })();
